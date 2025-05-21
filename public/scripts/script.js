7211b6e0df9619e9cd89fe8ad38c61bbfcfd1c99
Brancovanbeek <branco.van.beek@hva.nl>
2025-05-21 13:01:36 +0200

    feat: add leden and lid pagina, #56

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -15,3 +15,18 @@ const closeMenu = document.getElementById('toggle-menu-close');
         const header = document.querySelector('.header');
         header.classList.toggle('nav-open')
     })
+
+
+const searchInputs = document.querySelectorAll('input[type="text"][id="search"]'); 
+
+searchInputs.forEach(search => {
+    search.addEventListener('input', function () {
+        const searchValue = this.value.toLowerCase();
+        const leden = document.querySelectorAll('.leden-card-grid li');
+
+        leden.forEach(lid => {
+            const title = lid.querySelector('h2').textContent.toLowerCase();
+            lid.style.display = title.includes(searchValue) ? '' : 'none';
+        });
+    });
+});
