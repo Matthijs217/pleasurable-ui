a53acbbe522f02719d9969ee810e2d89491f0df0
Matthijs217 <matthijstenbrink@gmail.com>
2025-05-22 14:03:04 +0200

    Stack animatie af #60
    Animatie mooi af gemaakt zoals ik hem wou.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,20 +1,25 @@
 const openMenu = document.getElementById('toggle-menu-open');
+const closeMenu = document.getElementById('toggle-menu-close');
 
     openMenu.addEventListener('click', function(event){
         event.preventDefault();
 
         const header = document.querySelector('.header');
-        header.classList.toggle('nav-open')
-    })
+        const menu = document.querySelector('#menu');
 
-const closeMenu = document.getElementById('toggle-menu-close');
+        header.classList.toggle('nav-open');
+        menu.classList.toggle('open');
+    });
 
     closeMenu.addEventListener('click', function(event){
         event.preventDefault();
 
         const header = document.querySelector('.header');
-        header.classList.toggle('nav-open')
-    })
+        const menu = document.querySelector('#menu');
+
+        header.classList.toggle('nav-open');
+        menu.classList.remove('open');
+    });
 
 
 const searchInputs = document.querySelectorAll('input[type="text"][id="search"]'); 
